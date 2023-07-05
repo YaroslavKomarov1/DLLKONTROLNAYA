02cab2ef576216562d5f38e3debd0f5ff62af4fb
choch <chtoblin3@gmail.ru>
2023-07-05 10:32:32 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Product.cs
@@ -0,0 +1,27 @@
+﻿// Product.cs Выполнено студентом Комаровым Ярославом Вячеславовичем 34ИС
+using System;
+
+namespace KomarovYaroslav.Library
+{
+    public class Product : IComparable
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}, Number: {Number}");
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Product otherProduct = obj as Product;
+            if (otherProduct != null)
+                return this.Number.CompareTo(otherProduct.Number);
+            else
+                throw new ArgumentException("Etot object - ne produkt");
+        }
+    }
+}
---
+++ ClassLibrary1/Store.cs
@@ -0,0 +1,26 @@
+﻿// Store.cs  Выполнено студентом Комаровым Ярославом Вячеславовичем 34ИС
+using System;
+using System.Collections.Generic;
+namespace KomarovYaroslav.Library
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Store()
+        {
+            Products = new List<Product>();
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Name: {Name}, Address: {Address}");
+            foreach (Product product in Products)
+            {
+                product.Show();
+            }
+        }
+    }
+}
